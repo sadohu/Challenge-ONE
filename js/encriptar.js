4e3c9e3a3eff32776ad4358bcb5c29586cbc5a4e
sadohu <sadohu26@yahoo.com>
2023-05-01 14:49:52 -0500

    Encriptador base realizado.

--- js/encriptar.js
+++ js/encriptar.js
@@ -4,8 +4,9 @@ var res = "";
 function imprimir(){
     msg = document.querySelector("#msg").value;
     res = encriptador(msg);
+    console.log(res); 
     document.querySelector("#result").value = res;
-    //limpiar();
+    limpiar();
 }
 
 function limpiar(){
@@ -13,12 +14,43 @@ function limpiar(){
 }
 
 function encriptador(cad){
-    var chr = "";
-    var msg = "";
+    console.log(cad); 
+    var chr, msg, modif = "";
+    console.log(msg); 
     for(var i = 0; i < cad.length; i++){
         chr = cad[i];
-        console.log(chr);
-        msg += chr;
+        console.log(chr); 
+        switch(chr){
+            case "a":{
+                modif = "ai";
+                break;
+            }
+            case "e":{
+                modif = "enter";
+                break;
+            }
+            case "i":{
+                modif = "imes";
+                break;
+            }
+            case "o":{
+                modif = "ober";
+                break;
+            }
+            case "u":{
+                modif = "ufat";
+                break;
+            }
+            default:{
+                modif = chr;
+                break;
+            }
+        }
+        if(i == 0){
+            msg = modif;
+        }else{
+            msg += modif;
+        }
     }
     return msg;
 }
